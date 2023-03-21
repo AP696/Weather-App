4aba4b3f2ee35ae90376e1493e01b3fc235a7b3a
AP696 <addyphilips66@gmail.com>
2023-03-21 14:05:38 +0000

    basics done

--- main.py
+++ main.py
@@ -1,3 +1,30 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, request
+import requests
 
 app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        city = request.form['city']
+        weather_data = get_weather_data(city)
+        return render_template('base.html', weather=weather_data)
+    return render_template('base.html')
+
+def get_weather_data(city):
+    API_KEY = '068b9448c8d37f37d18dd401e55f8aec'
+    URL = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}'
+    response = requests.get(URL)
+    data = response.json()
+    if data['cod'] == 200:
+        weather_data = {
+            'city': data['name'],
+            'temperature': data['main']['temp'],
+            'description': data['weather'][0]['description'],
+            'icon': data['weather'][0]['icon']
+        }
+        return weather_data
+
+    
+if __name__ == '__main__':
+    app.run(debug=True)
